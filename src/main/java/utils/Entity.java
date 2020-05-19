a92c3e501ae79ea4dc59448a134d474fed47d99a
Bomba <pablo.gun.66@gmail.com>
2020-05-18 22:24:27 -0400

    build(Entity): Creacion de una clase que convierte un objeto a su representacion en String.

---
+++ src/main/java/utils/Entity.java
@@ -0,0 +1,46 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Pablo Salas Olivares <pablo.salas@ucn.cl>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+/**
+ * Entity/Domain utils
+ *
+ * @author Pablo Salas Olivares
+ */
+package utils;
+
+import org.apache.commons.lang3.builder.MultilineRecursiveToStringStyle;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+public final class Entity {
+
+    /**
+     * Convierte cualquier objeto en representacion String
+     *
+     *
+     * @param object a convertir a String
+     * @return el String
+     */
+    public static String toString(Object object) {
+        return ReflectionToStringBuilder.toString(object, new MultilineRecursiveToStringStyle());
+    }
+}
